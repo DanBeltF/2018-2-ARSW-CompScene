47df814b03b614902ba81bf745458fd629d15ce7
unknown <2098325@labinfo.is.escuelaing.edu.co>
2018-10-24 12:02:05 -0500

    AVANCE EN UNA SUBCLASE Y VISTA

--- src/main/java/edu/eci/arsw/compscene/model/impl/PreguntaRellenar.java
+++ src/main/java/edu/eci/arsw/compscene/model/impl/PreguntaRellenar.java
@@ -24,6 +24,7 @@
 package edu.eci.arsw.compscene.model.impl;
 
 import edu.eci.arsw.compscene.model.Pregunta;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
 
@@ -34,76 +35,187 @@ import java.util.Timer;
 public class PreguntaRellenar extends Pregunta{
     
     private List<String> respuestaCorrecta;
-
+    private Boolean  verdad=true;
+    
+    /**
+     *Preguntas a Rellenar
+     * 
+     * <p>Use {@link #Sirve para almacenar las respuestas que son escritas por el usuario.*
+     * 
+     * @param respuestaCorrecta La respuesta correcta de la pregunta.  
+     * @param id    El id de la pregunta.
+     * @param enunciado El enunciado de la pregunta.
+     * @param tema  El tema de la pregunta
+     * @param opcionesDeRespuesta   Las respuestas del Usuario.
+     * @param tiempo    El tiempo que se tomo el usuario en responder.
+     */
     public PreguntaRellenar(List<String> respuestaCorrecta, int id, String enunciado, String tema, List<String> opcionesDeRespuesta, Timer tiempo) {
         super(id, enunciado, tema, opcionesDeRespuesta, tiempo);
         this.respuestaCorrecta = respuestaCorrecta;
     }
 
+    /**
+     *Validador de Respuesta
+     * <p>Use {@link #.Valida la respuesta del usuario con las respuestas.* 
+     * @param respuestaJugador  El arrreglo de respuestas del usuario.
+     * @param respuestaCorrecta El arreglo de respuestas correctas.
+     * @return Un Booleano con el valor true o false si el usuario se a equivocado.
+     */
     @Override
-    public boolean validadorRespuesta(Object respuestaJugador, Object respuestaCorrecta) {
-        return super.validadorRespuesta(respuestaJugador, respuestaCorrecta); //To change body of generated methods, choose Tools | Templates.
+    public boolean validadorRespuesta(Object respuestaJugador, Object respuestaCorrecta){
+        ArrayList<String> respJugador=(ArrayList<String>) respuestaJugador;
+        ArrayList<String> respCorr=(ArrayList<String>) respuestaCorrecta;
+        int respcor=0;
+        int a =0;
+        int b=0;
+        while(verdad && a<respJugador.size()&& b<respCorr.size()){
+            if(respJugador.get(a).equals(respCorr.get(b))){
+                a++;
+                b++;  
+                respcor++;
+            }
+            a++;
+            b++;              
+        }
+        if(respcor>=respJugador.size()/2){
+            verdad=true;
+        }else{
+            verdad=false;
+        }
+        return verdad; //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     *GetRespuestaJugador
+     * <p>Use {@link #Sirve para devolver las respuestas que son escritas por el usuario.*
+     * @return Una lista con las respuestas del usuario.
+     */
     @Override
     public Object getRespuestaJugador() {
         return super.getRespuestaJugador(); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     *SetTeimpo
+     * <p>Use {@link #Asigna un valor al tiempo del usuario..*
+     * @param tiempo El tiempo del usuario en responder una pregunta.
+     */
     @Override
     public void setTiempo(Timer tiempo) {
         super.setTiempo(tiempo); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     *getTiempo
+     * <p>Use {@link #Sirve para retornar el tiempo que el usuario a tomado para responder la pregunta*
+     * 
+     * @return El tiempo del usuario.
+     */
     @Override
     public Timer getTiempo() {
         return super.getTiempo(); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     *
+     * <p>Use {@link #Sirve para almacenar las respuestas correctas de las preguntas.*
+     * 
+     * @param opcionesDeRespuesta  Asigna las respuestas correctas de las preguntas.
+     */
     @Override
     public void setOpcionesDeRespuesta(List<String> opcionesDeRespuesta) {
         super.setOpcionesDeRespuesta(opcionesDeRespuesta); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     *getOpciones de respuesta
+     * <p>Use {@link #Sirve Devolver las respuestas correctas de las preguntas.*
+     * @return Una lista de String con las opciones correctas de las preguntas.
+     */
     @Override
     public List<String> getOpcionesDeRespuesta() {
         return super.getOpcionesDeRespuesta(); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     *setTema
+     * <p>Use {@link #Establece un tema para una pregunta. *
+     * @param tema El tema de la pregunta.
+     */
     @Override
     public void setTema(String tema) {
         super.setTema(tema); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     *getTema
+     * <p>Use {@link #Regresa el tema de la pregunta.*
+     * @return Un string con el tema de la pregunta.
+     */
     @Override
     public String getTema() {
         return super.getTema(); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     *setEnunciado
+     * <p>Use {@link #Sirve para almacenar el enunciado de la pregunta.* 
+     * 
+     * @param enunciado Es un string con el enunciado de la preguntado.
+     */
     @Override
     public void setEnunciado(String enunciado) {
         super.setEnunciado(enunciado); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     *getEnunciado
+     * <p>Use {@link #Sirve para obtener el enunciado de la pregunta.*
+     * 
+     * @return Un string con el enunciado de la pregunta.
+     */
     @Override
     public String getEnunciado() {
         return super.getEnunciado(); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     *setId
+     * <p>Use {@link #.Sirve para establecer un id a la pregunta*
+     * 
+     * @param id Un entero que representa el identificador de la pregunta.
+     */
     @Override
     public void setId(int id) {
         super.setId(id); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     *getId
+     * <p>Use {@link #Sirve para regresar el identificador de la pregunta.*
+     * @return un entero con el identificador de la pregunta.
+     */
     @Override
     public int getId() {
         return super.getId(); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     *getRespuestaCorrecta
+     * <p>Use {@link # Regresa la lista de respuestas correctas.* 
+     * 
+     * @return Retorna una lista de String con la lista de respuesta correctas.
+     */
     public List<String> getRespuestaCorrecta() {
         return respuestaCorrecta;
     }
 
+    /**
+     *setRespuestaCorrecta
+     * <p>Use {@link #Sirve para almacenar las respuestas correctas del cuestionario. *
+     * 
+     * @param respuestaCorrecta Una lista con las respuestas correctas del cuestionario.
+     */
     public void setRespuestaCorrecta(List<String> respuestaCorrecta) {
         this.respuestaCorrecta = respuestaCorrecta;
     }
