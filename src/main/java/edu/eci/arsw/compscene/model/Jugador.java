6ef96832449624d18ac86497aec55e0132148564
d.castiblanco <dolardanies@hotmail.com>
2018-10-24 14:18:33 -0500

    Promedio tiempos

--- src/main/java/edu/eci/arsw/compscene/model/Jugador.java
+++ src/main/java/edu/eci/arsw/compscene/model/Jugador.java
@@ -26,12 +26,14 @@ package edu.eci.arsw.compscene.model;
 import edu.eci.arsw.compscene.persistence.impl.Tripla;
 import edu.eci.arsw.compscene.persistence.impl.Tupla;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  *
  * @author dcastiblanco
  */
 public class Jugador {
+
     private int id;
     private String nombre;
     private String password;
@@ -50,55 +52,100 @@ public class Jugador {
         this.password = password;
         this.puntajeJugador = puntajeJugador;
     }
-    
+
     /**
-     * Calcula el puntaje específico del jugador 
-     * @param respuestas Lista de respuestas que contienen tema, respuesta y el tiempo ejecutado en responder
-     * @return  Puntaje de cada jugador por ronda
+     * Calcula el puntaje específico del jugador
+     *
+     * @param respuestas Lista de respuestas que contienen tema, respuesta y el
+     * tiempo ejecutado en responder
+     * @return Puntaje de cada jugador por ronda
      */
-    public float calcularPuntajeEspecifico(List<Tripla<String,Boolean,Float>> respuestas){
-        float correctas=0; 
-        for (int i=0; i<respuestas.size(); i++){
-            if(respuestas.get(i).getElem2()==true){
-                correctas+=100;
-                correctas=correctas-respuestas.get(i).getElem3();                
-            }            
+    public float calcularPuntajeEspecifico(List<Tripla<String, Boolean, Float>> respuestas) {
+        float correctas = 0;
+        for (int i = 0; i < respuestas.size(); i++) {
+            if (respuestas.get(i).getElem2() == true) {
+                correctas += 100;
+                correctas = correctas - respuestas.get(i).getElem3();
+            }
         };
         return correctas;
     }
-    
+
     
     /**
      * Calcula el puntaje del jugador por tema
-     * @param respuestas Lista de respuestas que contienen tema, respuesta y el tiempo ejecutado en responder
-     * @return Lista de tuplas que contienen los temas y sus respectivos puntajes
+     *
+     * @param respuestas Lista de respuestas que contienen tema, respuesta y el
+     * tiempo ejecutado en responder
+     * @return Lista de tuplas que contienen los temas y sus respectivos
+     * puntajes
      */
-    public List<Tupla<String,Float>> calcularPuntajePorTema(List<Tripla<String,Boolean,Float>> respuestas){
-        return null;
+    public List<Tupla<String, Float>> calcularPuntajePorTema(List<Tripla<String, Boolean, Float>> respuestas) {
+        List<Tupla<String, Float>> puntajeTemas = new CopyOnWriteArrayList<Tupla<String, Float>>();
+        Tupla<String, Float> matematica; 
+        Tupla<String, Float> logica;
+        Tupla<String, Float> programacion;
+        float correctaMate = 0;
+        float correctaLogi = 0;
+        float correctaProg = 0;
+        for (int i = 0; i < respuestas.size(); i++) {
+            if (null != respuestas.get(i).getElem1()) switch (respuestas.get(i).getElem1()) {
+                case "Matematica":
+                    if (respuestas.get(i).getElem2() == true) {
+                        correctaMate += 100;
+                        correctaMate = correctaMate - respuestas.get(i).getElem3();
+                    }   break;
+            
+                case "Lógica":
+                    if (respuestas.get(i).getElem2() == true) {
+                        correctaLogi += 100;
+                        correctaLogi = correctaLogi - respuestas.get(i).getElem3();
+                    }   break;
+                case "Programación":
+                    if (respuestas.get(i).getElem2() == true) {
+                        correctaProg += 100;
+                        correctaProg = correctaProg - respuestas.get(i).getElem3();
+                    }   break;
+                default:
+                    break;
+            }
+        }
+        //Agregar puntajes y temas a la tuplas respectivas
+        //Agregar tuplas a las lista puntajeTemas
+
+        return puntajeTemas;
     }
+
+    
+    /**
+     * Calcula el tiempo promedio en responder todas las preguntas
+     * @param respuestas Lista de respuestas que contienen tema, respuesta y el
+     * tiempo ejecutado en responder
+     * @return Un float con el tiempo promedio de respuesta
+     */
+    public float calcularTiempoPromedioEspecifico(List<Tripla<String, Boolean, Float>> respuestas) {        
+        float tiempos=0;
+        for(int i=0; i<respuestas.size();i++){
+            tiempos+=respuestas.get(i).getElem3();
+        }
+        float promedio=tiempos/respuestas.size();
+        return promedio;
+    }
+
     
     /**
      *
-     * @param respuestas
      * @return
      */
-    public float calcularTiempoPromedioEspecifico(List<Tripla<String,Boolean,Float>> respuestas){
+    public float calcularTematicaMasAcertada() {
         return 0;
     }
-    
+
     /**
      *
      * @return
      */
-    public float calcularTematicaMasAcertada(){
-        return 0;
-    }
-    
-    /**
-     *
-     * @return
-     */
-    public float calcularTematicaMasFallada(){
+    public float calcularTematicaMasFallada() {
         return 0;
     }
 
@@ -170,7 +217,5 @@ public class Jugador {
     public String toString() {
         return "Jugador{" + "id=" + id + ", nombre=" + nombre + ", password=" + password + ", puntajeJugador=" + puntajeJugador + '}';
     }
-    
-    
-    
+
 }
