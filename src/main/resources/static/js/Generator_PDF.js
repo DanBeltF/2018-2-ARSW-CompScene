ba3954d800b0c1ca3489500e326365a5cee06f2c
cesareduardo <cesareduardo824@gmail.com>
2018-12-10 02:48:35 -0500

    Mejora v2

--- src/main/resources/static/js/Generator_PDF.js
+++ src/main/resources/static/js/Generator_PDF.js
@@ -10,7 +10,7 @@ var getPDF = (function pdf() {
     
     
     
-    function pd(){
+  function pd(){
         var img= new Image();
         
         img.addEventListener('load', function() {
@@ -24,8 +24,11 @@ var getPDF = (function pdf() {
         
         var doc = new jsPDF('landscape');
         doc.text(20, 20, 'Hola, estos son tus resultados');
-        doc.addImage(img, 'png', 10, 50);
-        
+        doc.addImage(img, 'png', 10, 60);
+        doc.text(30, 25, 'El resultado individual de respuestas correctas es: ');        
+        doc.text(30, 35, 'El resultado grupal de respuestas correctas es: ');        
+        doc.text(30, 40, 'El resultado individual de respuestas erroneas es: ');        
+        doc.text(30, 45, 'El resultado grupal de respuestas erroneas es: ');                
         doc.text(30, 30, 'FELICIDADES');
 
         doc.save('Test.pdf');
