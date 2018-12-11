97cc3e6133f63f74f7d053c36a9b13efe76990de
unknown <admin@sistemas108.labinfo.is.escuelaing.edu.co>
2018-12-11 15:50:19 -0500

    Pruebas unitarias basicas

--- src/test/java/edu/eci/arsw/compscene/test/CompScenceServicesTests.java
+++ src/test/java/edu/eci/arsw/compscene/test/CompScenceServicesTests.java
@@ -24,8 +24,17 @@
 package edu.eci.arsw.compscene.test;
 
 import edu.eci.arsw.compscene.model.Cuestionario;
+import edu.eci.arsw.compscene.model.Jugador;
+import edu.eci.arsw.compscene.model.Pregunta;
+import edu.eci.arsw.compscene.model.Resultado;
+import edu.eci.arsw.compscene.model.impl.PreguntaRellenar;
+import edu.eci.arsw.compscene.persistence.impl.Tripla;
+import edu.eci.arsw.compscene.persistence.impl.Tupla;
 import edu.eci.arsw.compscene.services.CompSceneServices;
 import edu.eci.arsw.compscene.services.CompSceneServicesException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.junit.Test;
@@ -35,6 +44,13 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.apache.maven.surefire.booter.ForkedBooter;
+import org.assertj.core.groups.Tuple;
+import org.junit.Assert;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertSame;
 import static org.quicktheories.QuickTheory.qt;
 import static org.quicktheories.generators.Generate.*;
 import static org.quicktheories.generators.SourceDSL.*;
@@ -50,7 +66,15 @@ public class CompScenceServicesTests {
     
     @Autowired
     private CompSceneServices compserv;
+    private PreguntaRellenar pr;
+        private PreguntaRellenar pr1;
+    private List<String> respuestaCorrectaRellenar1;
+    private  List<String> respuestaCorrectaRellenar;
     
+    private List<Tripla<String, Boolean, Float>> respuestas;
+    
+    private List<Pregunta> listaPreguntas;
+    private List<Jugador> jugador;
     @Test
     public void contextLoads() throws CompSceneServicesException {
     }
@@ -60,4 +84,84 @@ public class CompScenceServicesTests {
        
         
     }
+    
+    @Test
+    public void modeloC()throws CompSceneServicesException {
+        jugador= new CopyOnWriteArrayList<>();
+        List<Tupla<String, Float>>puntajeTemas =new ArrayList<Tupla<String, Float>>();
+          
+        Tupla t=new Tupla("MATEMATICAS", 12.0);
+        Tupla t1=new Tupla("Programacion", 15.0);
+        Tupla t2=new Tupla("Logica", 5.0);        
+        puntajeTemas.add(t);
+        puntajeTemas.add(t1);
+        puntajeTemas.add(t2);
+        
+        Jugador jug1= new Jugador (12, "Daniel", "duq", 12.2f, puntajeTemas);
+        Jugador jug2= new Jugador (13, "beltran", "duq", 12.2f, puntajeTemas);
+        Jugador jug3= new Jugador (15, "Cesar", "duq", 12.2f, puntajeTemas);
+        respuestas = new CopyOnWriteArrayList<>();
+        listaPreguntas= new CopyOnWriteArrayList<>();
+        respuestaCorrectaRellenar= new CopyOnWriteArrayList<>();
+        respuestaCorrectaRellenar.add("22.3");
+        respuestaCorrectaRellenar.add("10");
+        pr = new PreguntaRellenar(respuestaCorrectaRellenar,1,"Raiz cuadrada de 500 es {} y la de 100 es {}", "Matemática", null, 6.7f);
+        pr1 = new PreguntaRellenar(respuestaCorrectaRellenar,1,"La derivada de x2 es {} ", "Matemática", null, 6.7f);
+        respuestaCorrectaRellenar.add("2x");
+        listaPreguntas.add(pr);
+        //jugador
+        jugador.add(jug1);
+        jugador.add(jug2);
+        jugador.add(jug3);
+        List<Tripla<String, Boolean, Float>>  tripla=new CopyOnWriteArrayList<>(); 
+        
+        Tripla tr1=new Tripla ("LA matematica es delociosa",true,15.0f);
+        Tripla tr2=new Tripla ("Esto es de otro mundo",false,12.0f);
+        tripla.add(tr1);
+        tripla.add(tr2);
+        Cuestionario c1= new Cuestionario(1, listaPreguntas, jugador,tripla);
+
+       assertSame("Sorpresa",jugador,c1.getJugadores());
+        
+    
+    
+    }
+    
+     @Test
+    public void modeloJ()throws CompSceneServicesException {
+        List<Tupla<String, Float>>puntajeTemas =new ArrayList<Tupla<String, Float>>();  
+        Tupla t=new Tupla("MATEMATICAS", 12.0);
+        Tupla t1=new Tupla("Programacion", 15.0);
+        Tupla t2=new Tupla("Logica", 5.0);        
+        puntajeTemas.add(t);
+        puntajeTemas.add(t1);
+        puntajeTemas.add(t2);
+        List<Tripla<String, Boolean, Float>>  tripla=new CopyOnWriteArrayList<>(); 
+        
+        Tripla tr1=new Tripla ("LA matematica es delociosa",true,125.0f);
+        Tripla tr2=new Tripla ("Esto es de otro mundo",false,11.0f);
+        tripla.add(tr1);
+        tripla.add(tr2);        
+        
+        Jugador j1=new Jugador(1, "Daniel", "Beltran", 5.0f, puntajeTemas);
+    
+         Assert.assertEquals(-25.0f, j1.calcularPuntajeEspecifico(tripla), 0);
+        
+    }
+    @Test
+    public void modeloR()throws CompSceneServicesException {
+        Resultado re= new Resultado(12);
+        
+        
+        List<Tupla<String, Float>>puntajeTemas =new ArrayList<Tupla<String, Float>>();  
+        Tupla t=new Tupla("MATEMATICAS", 12.0f);
+        Tupla t1=new Tupla("Programacion", 15.0f);
+        Tupla t2=new Tupla("Logica", 5.0f);        
+        puntajeTemas.add(t);
+        puntajeTemas.add(t1);
+        puntajeTemas.add(t2);
+        Assert.assertEquals(32.0f, re.calcularPuntajeGeneral(puntajeTemas), 0);
+        
+    }
+    
 }
